417b73a829687ef45301f51f8a549a4d952edf94
deyu <deyu.zhang@tendcloud.com>
2023-04-05 22:08:22 +0800

    2023-04-05 1.增加测试文件

--- src/test/Test.py
+++ src/test/Test.py
@@ -8,3 +8,4 @@ if __name__ == '__main__':
     USER_CONFIG_DIR = getenv('USER_CONFIG_DIR', user_config_dir('Pandora-ChatGPT'))
     token_file = os.path.join(USER_CONFIG_DIR, 'access_token.dat')
     print(token_file)
+1
